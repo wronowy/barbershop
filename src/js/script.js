7aa2bc547d03dc630c61bcc46b384111d9c65aca
Adam <wronowyadam@gmail.com>
2023-12-14 23:00:25 +0800

    edited index, added section offers

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,23 @@
+// const menuMobile = document.querySelector('.menu-hamburger')
+// const navLinks = document.querySelector('.nav-links')
+// const mobileLink = document.querySelectorAll('.nav-link')
+// const mobileMenu = document.querySelector('.mobile-menu')
+
+// menuMobile.addEventListener('click', () => {
+// 	navLinks.classList.toggle('mobile-menu')
+// })
+
 const menuMobile = document.querySelector('.menu-hamburger')
 const navLinks = document.querySelector('.nav-links')
+const mobileLink = document.querySelectorAll('.nav-link')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 menuMobile.addEventListener('click', () => {
 	navLinks.classList.toggle('mobile-menu')
+})
+
+mobileLink.forEach(link => {
+	link.addEventListener('click', () => {
+		navLinks.classList.remove('mobile-menu')
+	})
 })
